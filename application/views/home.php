98dd7eae2914a82c17820c4d4b9cebd993ba33de
Daniel Cunha <158943@upf.br>
2017-10-19 10:30:46 -0200

    Criado um template default com base no template da W3Schools. Também foi criada a pasta assets com o intuito de guardar arquivos da aplicação, como a logo e o ícone. Além disso, foi mudado a rota padrão da aplicação para o controller Home

---
+++ application/views/home.php
@@ -0,0 +1,121 @@
+<!-- Header -->
+<header id="portfolio">
+    <a href="#"><img src="/w3images/avatar_g2.jpg" style="width:65px;" class="w3-circle w3-right w3-margin w3-hide-large w3-hover-opacity"></a>
+    <span class="w3-button w3-hide-large w3-xxlarge w3-hover-text-grey" onclick="w3_open()"><i class="fa fa-bars"></i></span>
+    <div class="w3-container">
+        <h1><b>Portfolio</b></h1>
+        <div class="w3-section w3-bottombar w3-padding-16">
+            <button class="w3-button w3-black">TODOS</button>
+            <button class="w3-button w3-white"></i>Lítico</button>
+            <button class="w3-button w3-white">Cerâmica</button>
+            <button class="w3-button w3-white">Outro</button>
+        </div>
+    </div>
+</header>
+
+<!-- First Photo Grid-->
+<div class="w3-row-padding">
+    <div class="w3-third w3-container w3-margin-bottom">
+        <img src="assets/images/img1.jpg" alt="Imagem 1" style="width:100%" class="w3-hover-opacity">
+        <div class="w3-container w3-white">
+            <p><b>Lorem Ipsum</b></p>
+            <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+        </div>
+    </div>
+
+    <div class="w3-third w3-container w3-margin-bottom">
+        <img src="assets/images/img2.jpg" alt="Imagem 2" style="width:100%" class="w3-hover-opacity">
+        <div class="w3-container w3-white">
+            <p><b>Lorem Ipsum</b></p>
+            <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+        </div>
+    </div>
+
+    <div class="w3-third w3-container">
+        <img src="assets/images/img3.jpg" alt="Imagem 3" style="width:100%" class="w3-hover-opacity">
+        <div class="w3-container w3-white">
+            <p><b>Lorem Ipsum</b></p>
+            <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+        </div>
+    </div>
+</div>
+
+<!-- Second Photo Grid-->
+<div class="w3-row-padding">
+    <div class="w3-third w3-container w3-margin-bottom">
+        <img src="assets/images/img4.jpg" alt="Imagem 4" style="width:100%" class="w3-hover-opacity">
+        <div class="w3-container w3-white">
+            <p><b>Lorem Ipsum</b></p>
+            <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+        </div>
+    </div>
+
+    <div class="w3-third w3-container w3-margin-bottom">
+        <img src="assets/images/img5.jpg" alt="Imagem 5" style="width:100%" class="w3-hover-opacity">
+        <div class="w3-container w3-white">
+            <p><b>Lorem Ipsum</b></p>
+            <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+        </div>
+    </div>
+
+    <div class="w3-third w3-container">
+        <img src="assets/images/img6.jpg" alt="Imagem 6" style="width:100%" class="w3-hover-opacity">
+        <div class="w3-container w3-white">
+            <p><b>Lorem Ipsum</b></p>
+            <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+        </div>
+    </div>
+</div>
+
+<!-- Pagination -->
+<!-- <div class="w3-center w3-padding-32">
+<div class="w3-bar">
+<a href="#" class="w3-bar-item w3-button w3-hover-black">«</a>
+<a href="#" class="w3-bar-item w3-black w3-button">1</a>
+<a href="#" class="w3-bar-item w3-button w3-hover-black">2</a>
+<a href="#" class="w3-bar-item w3-button w3-hover-black">3</a>
+<a href="#" class="w3-bar-item w3-button w3-hover-black">4</a>
+<a href="#" class="w3-bar-item w3-button w3-hover-black">»</a>
+</div>
+</div> -->
+
+<div class="w3-container w3-padding-large" style="margin-bottom:32px" id="about">
+    <h4><b>Sobre</b></h4>
+    <p>Descrição do projeto. Just me, myself and I, exploring the universe of unknownment. I have a heart of love and an interest of lorem ipsum and mauris neque quam blog. I want to share my world with you. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+</div>
+
+<!-- Contact Section -->
+<div class="w3-container w3-padding-large w3-grey">
+    <h4 id="contact"><b>Contato</b></h4>
+    <div class="w3-row-padding w3-center w3-padding-24" style="margin:0 -16px">
+        <div class="w3-third w3-dark-grey">
+            <p><i class="fa fa-envelope w3-xxlarge w3-text-light-grey"></i></p>
+            <p>victorbilly@upf.br</p>
+        </div>
+        <div class="w3-third w3-teal">
+            <p><i class="fa fa-map-marker w3-xxlarge w3-text-light-grey"></i></p>
+            <p>Passo Fundo, RS</p>
+        </div>
+        <div class="w3-third w3-dark-grey">
+            <p><i class="fa fa-phone w3-xxlarge w3-text-light-grey"></i></p>
+            <p>(54) 3316-5804</p>
+        </div>
+    </div>
+    <hr class="w3-opacity">
+    <form action="/action_page.php" target="_blank">
+        <div class="w3-section">
+            <label>Nome</label>
+            <input class="w3-input w3-border" type="text" name="Name" required>
+        </div>
+        <div class="w3-section">
+            <label>E-mail</label>
+            <input class="w3-input w3-border" type="text" name="Email" required>
+        </div>
+        <div class="w3-section">
+            <label>Mensagem</label>
+            <!-- <input class="w3-input w3-border" type="text" name="Message" required> -->
+            <textarea class="w3-input w3-border" type="text" name="Message" required></textarea>
+        </div>
+        <button type="submit" class="w3-button w3-black w3-margin-bottom"><i class="fa fa-paper-plane w3-margin-right"></i>Enviar mensagem</button>
+    </form>
+</div>
