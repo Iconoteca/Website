1d25c6d6a76d7fde1f1723fde7846267700d38fd
Daniel Cunha <158943@upf.br>
2017-10-24 10:01:35 -0200

    Adicionado o parâmetro numArtefatos ao index do controller Home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,13 +1,34 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Home extends CI_Controller {
+class Home extends CI_Controller
+{
 
-	public function index()
+	public function index($numArtefatos = 0)
 	{
-		$dados['title']  = "Iconoteca";
-		$dados['icones'] = $this->m_icone->get("icones");
+		if($numArtefatos != 0)
+		{
+			echo "<h1>NICE</h1>";
+		}
+		else {
+			$dados['title']  = "Iconoteca";
+			$dados['icones'] = $this->m_icone->get("icones");
 
-		$this->template->load('templates/default', 'home', $dados);
+			// Paginação
+			$this->load->library('pagination');
+
+			$config['base_url']   = base_url('home/');
+			$config['total_rows'] = $dados['icones']->num_rows();
+			$config['per_page']   = 3;
+			$config['num_links']  = 3;
+			$config['cur_tag_open'] = '<a href="#" class="w3-bar-item w3-black w3-button">';
+			$config['cur_tag_close'] = '</a>';
+			$config['attributes'] = array('class' => 'w3-bar-item w3-button w3-hover-black');
+			// $config['next_link'] = '&gt;';
+			// $config['prev_link'] = '&lt;';
+
+			$this->pagination->initialize($config);
+			$this->template->load('templates/default', 'home', $dados);
+		}
 	}
 }
